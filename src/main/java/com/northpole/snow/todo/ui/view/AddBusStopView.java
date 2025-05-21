d5bc2b0aafa4947098120954e42ce572fb7af7f7
Adam Matusiak <275460@student.pwr.edu.pl>
2025-05-21 22:06:34 +0200

    Connect busstop to db

---
+++ src/main/java/com/northpole/snow/todo/ui/view/AddBusStopView.java
@@ -0,0 +1,92 @@
+package com.northpole.snow.todo.ui.view;
+
+import com.northpole.snow.base.ui.component.ViewToolbar;
+import com.northpole.snow.todo.domain.Przystanek;
+import com.northpole.snow.todo.domain.PrzystanekRepository;
+import com.vaadin.flow.component.button.Button;
+import com.vaadin.flow.component.button.ButtonVariant;
+import com.vaadin.flow.component.html.Main;
+import com.vaadin.flow.component.notification.Notification;
+import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.textfield.TextArea;
+import com.vaadin.flow.router.PageTitle;
+import com.vaadin.flow.router.Route;
+import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Route("add-bus-stop")
+@PageTitle("Dodaj przystanek")
+@Menu(order = 4, icon = "vaadin:plus", title = "Dodaj przystanek")
+public class AddBusStopView extends Main {
+
+  private final PrzystanekRepository przystanekRepository;
+
+  @Autowired
+  public AddBusStopView(PrzystanekRepository przystanekRepository) {
+    this.przystanekRepository = przystanekRepository;
+
+    // Pole: nazwa przystanku
+    TextField stopName = new TextField("Nazwa przystanku");
+    stopName.setPlaceholder("Wprowadź nazwę przystanku");
+    stopName.setWidth("300px");
+
+    // Pole: ulica
+    TextField street = new TextField("Ulica");
+    street.setPlaceholder("Wprowadź ulicę");
+    street.setWidth("300px");
+
+    // Pole: położenie (opcjonalnie)
+    TextArea location = new TextArea("Położenie (opcjonalnie)");
+    location.setPlaceholder("Wprowadź położenie lub zostaw puste");
+    location.setWidth("300px");
+
+    // Przycisk zapisz przystanek
+    Button saveButton = new Button("Zapisz przystanek", e -> {
+      String nazwa = stopName.getValue();
+      String ulica = street.getValue();
+      String polozenie = location.getValue();
+
+      if (nazwa == null || nazwa.isBlank() || ulica == null || ulica.isBlank()) {
+        Notification.show("Nazwa i ulica są wymagane!", 3000, Notification.Position.MIDDLE);
+        return;
+      }
+
+      Przystanek przystanek = new Przystanek();
+      przystanek.setNazwa(nazwa);
+      przystanek.setUlica(ulica);
+      przystanek.setPolozenie(polozenie);
+
+      przystanekRepository.save(przystanek);
+
+      Notification.show("Dodano przystanek: " + nazwa, 3000, Notification.Position.MIDDLE);
+      stopName.clear();
+      street.clear();
+      location.clear();
+    });
+    saveButton.setWidth("300px");
+    saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+
+    // Layout pól formularza
+    VerticalLayout fieldsLayout = new VerticalLayout(
+        stopName,
+        street,
+        location);
+    fieldsLayout.setSpacing(true);
+    fieldsLayout.setPadding(false);
+
+    // Layout przycisku
+    HorizontalLayout buttonLayout = new HorizontalLayout(saveButton);
+
+    // Główny layout jak w tabletime (TimetableView)
+    VerticalLayout layout = new VerticalLayout(
+        new ViewToolbar("Dodaj przystanek"),
+        fieldsLayout,
+        buttonLayout);
+    layout.setSpacing(true);
+    layout.setPadding(true);
+
+    add(layout);
+  }
+}
--- src/main/java/com/northpole/snow/todo/ui/view/BusStopView.java
+++ src/main/java/com/northpole/snow/todo/ui/view/BusStopView.java
@@ -1,45 +1,69 @@
 package com.northpole.snow.todo.ui.view;
 
 import com.northpole.snow.base.ui.component.ViewToolbar;
+import com.northpole.snow.todo.domain.Przystanek;
+import com.northpole.snow.todo.domain.PrzystanekRepository;
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.button.ButtonVariant;
 import com.vaadin.flow.component.html.Main;
 import com.vaadin.flow.component.notification.Notification;
-import com.vaadin.flow.component.textfield.TextField;
+import com.vaadin.flow.component.combobox.ComboBox;
 import com.vaadin.flow.router.PageTitle;
 import com.vaadin.flow.router.Route;
 import com.vaadin.flow.theme.lumo.LumoUtility;
 import com.vaadin.flow.router.Menu;
+import com.vaadin.flow.component.orderedlayout.VerticalLayout;
+import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Route("bus-stops")
 @PageTitle("Manage Bus Stops")
 @Menu(order = 2, icon = "vaadin:map-marker", title = "Usuń przystanek")
 public class BusStopView extends Main {
 
-    public BusStopView() {
-        // Field for stop name
-        TextField stopName = new TextField("Nazwa przystanku");
-        stopName.setPlaceholder("Wprowadź nazwę przystanku");
-        stopName.setWidth("300px");
-        // Save button
-        Button saveButton = new Button("Save bus stop", e -> {
+    private final PrzystanekRepository przystanekRepository;
+
+    @Autowired
+    public BusStopView(PrzystanekRepository przystanekRepository) {
+        this.przystanekRepository = przystanekRepository;
+
+        // Pole wyboru przystanku
+        ComboBox<Przystanek> stopCombo = new ComboBox<>("Wybierz przystanek do usunięcia");
+        stopCombo.setItems(przystanekRepository.findAll());
+        stopCombo.setItemLabelGenerator(Przystanek::getNazwa);
+        stopCombo.setWidth("300px");
+
+        // Przycisk usuń przystanek
+        Button deleteButton = new Button("Usuń przystanek", e -> {
+            Przystanek selected = stopCombo.getValue();
+            if (selected != null) {
+                przystanekRepository.deleteById(selected.getId());
+                Notification.show("Usunięto przystanek: " + selected.getNazwa(), 3000, Notification.Position.MIDDLE);
+                stopCombo.setItems(przystanekRepository.findAll());
+                stopCombo.clear();
+            } else {
+                Notification.show("Wybierz przystanek do usunięcia", 3000, Notification.Position.MIDDLE);
+            }
         });
-        saveButton.setWidth("300px");
-        saveButton.addThemeVariants(ButtonVariant.LUMO_PRIMARY);
+        deleteButton.setWidth("300px");
+        deleteButton.addThemeVariants(ButtonVariant.LUMO_ERROR);
 
-        // Set up the main layout
-        setSizeFull();
-        addClassNames(
-                LumoUtility.BoxSizing.BORDER,
-                LumoUtility.Display.FLEX,
-                LumoUtility.FlexDirection.COLUMN,
-                LumoUtility.Padding.MEDIUM,
-                LumoUtility.Gap.SMALL);
+        // Layout pól formularza
+        VerticalLayout fieldsLayout = new VerticalLayout(stopCombo);
+        fieldsLayout.setSpacing(true);
+        fieldsLayout.setPadding(false);
 
-        // Add toolbar with title
-        add(new ViewToolbar("Usuń przystanek"));
+        // Layout przycisku
+        HorizontalLayout buttonLayout = new HorizontalLayout(deleteButton);
 
-        // Add form components
-        add(stopName, saveButton);
+        // Główny layout jak w AddBusStopView
+        VerticalLayout layout = new VerticalLayout(
+                new ViewToolbar("Usuń przystanek"),
+                fieldsLayout,
+                buttonLayout);
+        layout.setSpacing(true);
+        layout.setPadding(true);
+
+        add(layout);
     }
 }
